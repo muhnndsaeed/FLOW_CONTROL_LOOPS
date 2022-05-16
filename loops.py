8cadb139b7b6875dbb4ece6418fdc99790bd41fd
mhnd <mhnd@hno.com>
2022-05-16 12:45:53 +0300

    add loops.py

---
+++ loops.py
@@ -0,0 +1,17 @@
+
+from re import I
+
+
+for number in range(45,210):
+    print(number)
+
+
+
+
+
+while product= 168:
+print(input("what is the product of 7 * 24 ?"))
+if product== 168:
+    print("You answered this Question correctly")
+else :
+    print ("Your Answer is wrong try again.." )
